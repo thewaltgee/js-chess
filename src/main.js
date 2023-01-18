c0becaa48ebb44333613e687bc39ef9f47c882a4
Walt Gee <thewaltgee@gmail.com>
2023-01-18 22:46:44 +0000

    fix for script load error

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
-$(function() {
-	console.log("Main Init Called s1");
-	init();
-	console.log("Main Init Called s2");
-});
-
 function init() {
-	console.log("init() called");
+    console.log("init() called");
 }
+
+init();
