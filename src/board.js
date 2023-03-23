d95ce3b03d4c08376bb813468f041c738b4f49fa
Walt Gee <thewaltgee@gmail.com>
2023-03-23 23:28:41 +0000

    End of part 5

---
+++ src/board.js
@@ -0,0 +1,9 @@
+let Gameboard = {};
+
+Gameboard.pieces = new Array(BRD_SQ_NUM);
+Gameboard.side = COLOURS.WHITE;
+Gameboard.fiftyMove = 0;
+Gameboard.hisPly = 0;
+Gameboard.ply = 0;
+Gameboard.castlePerm = 0;
+
